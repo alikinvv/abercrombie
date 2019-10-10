b5934ebb4eb7bb46848db65c97836aa27b6a9133
Valery Alikin <weblcw@gmail.com>
2019-10-10 22:09:46 +0500

    update

--- build/js/main.js
+++ build/js/main.js
@@ -8,13 +8,31 @@ $('body').on('blur', '.header__controls input', (e) => {
     $(e.currentTarget).parent().removeClass('active');
 });
 
-var mySwiper = new Swiper ('.swiper-container', {
-    slidesPerView: 'auto',
-    spaceBetween: 20,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+$('.products__list.slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    swipe: false
+});
+
+$('.sliders .sliders__main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.sliders .sliders__nav',
+    swipe: false
+});
+$('.sliders .sliders__nav').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.sliders .sliders__main',
+    dots: false,
+    arrows: false,
+    centerMode: false,
+    focusOnSelect: true,
+    vertical: true,
+    swipe: false
 });
 
 $(window).on('scroll', () => {
@@ -37,4 +55,36 @@ $('body').on('click', '.toggle', (e) => {
     } else {
         $(e.currentTarget).addClass('active').next().slideDown(200);
     }
+});
+
+$('body').on('click', 'a[data-modal], div[data-modal]:not(.modal)', (e) => {
+    $('.modal').removeClass('active');
+    $('.modals').addClass('active');
+    $('.modal[data-modal="' + $(e.currentTarget).attr('data-modal') + '"]').addClass('active');
+});
+
+$('body').on('click', '.modal__close', () => {
+    $('.modals').removeClass('active');
+    $('.modal').removeClass('active');
+});
+
+$('body').on('click', '.modal[data-modal="success"] .btn', () => {
+    $('.modals').removeClass('active');
+    $('.modal').removeClass('active');
+});
+
+$('body').on('click', '.modals', (e) => {
+    if ($(e.target).is('.modals')) {
+        $('.modals').removeClass('active');
+        $('.modal').removeClass('active');
+    }
+});
+
+$('body').on('click', '.view__images div', (e) => {
+    $('.view__images div').removeClass('active');
+    $(e.currentTarget).addClass('active');
+});
+
+$('body').on('click', '.view__sizes-list div', (e) => {
+    $(e.currentTarget).toggleClass('active');
 });
