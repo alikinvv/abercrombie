5c550b7fb6c780a7328540fe48bba6b5176ce3ab
Valery Alikin <weblcw@gmail.com>
2019-10-10 17:28:47 +0500

    update

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,34 @@ $('body').on('click', '.header__controls form svg', (e) => {
 $('body').on('blur', '.header__controls input', (e) => {
     $(e.currentTarget).parent().removeClass('active');
 });
+
+var mySwiper = new Swiper ('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+$(window).on('scroll', () => {
+    if ( $('.scrolltop').length > 0) {
+        if ($(window).scrollTop() >= $(window).height() && $(window).width() >= 1280) {
+            $('.scrolltop').addClass('show');
+        } else {
+            $('.scrolltop').removeClass('show');
+        }
+    }
+});
+
+$('body').on('click', '.scrolltop', () => {
+    $('html, body').animate({scrollTop:0}, 'slow');
+});
+
+$('body').on('click', '.toggle', (e) => {
+    if ($(e.currentTarget).hasClass('active')) {
+        $(e.currentTarget).removeClass('active').next().slideUp(200);
+    } else {
+        $(e.currentTarget).addClass('active').next().slideDown(200);
+    }
+});
